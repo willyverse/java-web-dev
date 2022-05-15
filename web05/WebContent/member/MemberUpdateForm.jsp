0da36fb75351e6e97aeff2cd60c4bc1d10e2b61f
Chanyoung Kim <whqnd1106@gmail.com>
2022-05-15 23:29:34 +0900

    EL MemberList.jsp

--- web05/WebContent/member/MemberUpdateForm.jsp
+++ web05/WebContent/member/MemberUpdateForm.jsp
@@ -1,8 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<jsp:useBean id="member"
-		scope="request"
-		class="spms.vo.Member" />
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -10,17 +7,15 @@
 <title>회원정보</title>
 </head>
 <body>
-<h1>회원정보</h1>
-<form action="update" method="post">
-번호: <input type="text" name="no" value=<%=member.getNo()%> readonly><br>
-이름: <input type="text" name="name" value=<%=member.getName()%>><br>
-이메일: <input type="text" name="email" value=<%=member.getEmail()%>><br>
-가입일: <%=member.getCreatedDate()%><br>
-<input type="submit" value="저장">
-<input type="button"
-	value="삭제"
-	onclick="location.href='delete?no=<%=member.getNo()%>'">
-<input type="button" value="취소" onclick="location.href='list'">
-</form>
+	<h1>회원정보</h1>
+	<form action="update" method="post">
+		번호: <input type="text" name="no" value="${member.no}" readonly><br>
+		이름: <input type="text" name="name" value="${member.name}"><br>
+		이메일: <input type="text" name="email" value="${member.email}"><br>
+		가입일: ${member.createdDate}<br> <input type="submit" value="저장">
+		<input type="button" value="삭제"
+			onclick="location.href='delete?no=${member.no}'"> <input
+			type="button" value="취소" onclick="location.href='list'">
+	</form>
 </body>
 </html>
--- web05/src/spms/servlets/MemberUpdateServlet.java
+++ web05/src/spms/servlets/MemberUpdateServlet.java
@@ -22,23 +22,21 @@ import spms.vo.Member;
 @WebServlet("/member/update")
 public class MemberUpdateServlet extends HttpServlet {
 	@Override
-	protected void doGet(
-			HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
 			ServletContext sc = this.getServletContext();
-			conn = (Connection) sc.getAttribute("conn");   
+			conn = (Connection) sc.getAttribute("conn");
 			stmt = conn.createStatement();
-			rs = stmt.executeQuery(
-				"SELECT MNO,EMAIL,MNAME,CRE_DATE FROM MEMBERS" + 
-				" WHERE MNO=" + request.getParameter("no"));	
-			
+			rs = stmt
+					.executeQuery("SELECT MNO,EMAIL,MNAME,CRE_DATE FROM MEMBERS"
+							+ " WHERE MNO=" + request.getParameter("no"));
+
 			if (rs.next()) {
-				Member member = new Member()
-						.setNo(rs.getInt("MNO"))
+				Member member = new Member().setNo(rs.getInt("MNO"))
 						.setName(rs.getString("MNAME"))
 						.setEmail(rs.getString("EMAIL"))
 						.setCreatedDate(rs.getDate("CRE_DATE"));
@@ -46,52 +44,63 @@ public class MemberUpdateServlet extends HttpServlet {
 			} else {
 				throw new Exception("해당 번호의 회원이 존재하지 않습니당");
 			}
-			
+
 			response.setContentType("text/html; charset=UTF-8");
-			RequestDispatcher rd = request.getRequestDispatcher(
-					"/member/MemberUpdateForm.jsp");
+			RequestDispatcher rd = request
+					.getRequestDispatcher("/member/MemberUpdateForm.jsp");
 			rd.include(request, response);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			request.setAttribute("error", e);
 			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
 			rd.forward(request, response);
-			
+
 		} finally {
-			try {if (rs != null) rs.close();} catch(Exception e) {}
-			try {if (stmt != null) stmt.close();} catch(Exception e) {}
+			try {
+				if (rs != null)
+					rs.close();
+			} catch (Exception e) {
+			}
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (Exception e) {
+			}
 		}
 	}
-	
+
 	@Override
-	protected void doPost(
-			HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		try {
 			ServletContext sc = this.getServletContext();
-			conn = (Connection) sc.getAttribute("conn");    
-			stmt = conn.prepareStatement(
-					"UPDATE MEMBERS SET EMAIL=?,MNAME=?,MOD_DATE=now()"
-					+ " WHERE MNO=?");
+			conn = (Connection) sc.getAttribute("conn");
+			stmt = conn
+					.prepareStatement("UPDATE MEMBERS SET EMAIL=?,MNAME=?,MOD_DATE=now()"
+							+ " WHERE MNO=?");
 			stmt.setString(1, request.getParameter("email"));
 			stmt.setString(2, request.getParameter("name"));
 			stmt.setInt(3, Integer.parseInt(request.getParameter("no")));
 			stmt.executeUpdate();
-			
+
 			response.sendRedirect("list");
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			request.setAttribute("error", e);
 			RequestDispatcher rd = request.getRequestDispatcher("/Error.jsp");
 			rd.forward(request, response);
-			
+
 		} finally {
-			try {if (stmt != null) stmt.close();} catch(Exception e) {}
-			//try {if (conn != null) conn.close();} catch(Exception e) {}
+			try {
+				if (stmt != null)
+					stmt.close();
+			} catch (Exception e) {
+			}
+			// try {if (conn != null) conn.close();} catch(Exception e) {}
 		}
 	}
 }
