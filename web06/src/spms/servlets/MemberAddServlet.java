5a74c6a1e8572443584a4e945bafd52d1f552f99
Chanyoung Kim <whqnd1106@gmail.com>
2022-05-19 01:45:20 +0900

    mini MVC framework

---
+++ web06/src/spms/servlets/MemberAddServlet.java
@@ -0,0 +1,42 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+// ServletContext에 보관된 Connection 객체 사용  
+@WebServlet("/member/add")
+public class MemberAddServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("viewUrl", "/member/MemberForm.jsp");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		try {
+			ServletContext sc = this.getServletContext();
+			MemberDao memberDao = (MemberDao) sc.getAttribute("memberDao");
+			
+			Member member = (Member) request.getAttribute("member");
+			memberDao.insert(member);
+			
+			request.setAttribute("viewUrl", "redirect:list.do");
+
+		} catch (Exception e) {
+			throw new ServletException(e);
+		}
+	}
+}
---
+++ web06/src/spms/servlets/MemberUpdateServlet.java
@@ -0,0 +1,51 @@
+package spms.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import spms.dao.MemberDao;
+import spms.vo.Member;
+
+@WebServlet("/member/update")
+public class MemberUpdateServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		try {
+			ServletContext sc = this.getServletContext();
+			MemberDao memberDao = (MemberDao) sc.getAttribute("memberDao");
+
+			request.setAttribute("member", memberDao.selectOne(Integer
+					.parseInt(request.getParameter("no"))));
+			request.setAttribute("viewUrl", "/member/MemberUpdateForm.jsp");
+
+		} catch (Exception e) {
+			throw new ServletException();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			ServletContext sc = this.getServletContext();
+			MemberDao memberDao = (MemberDao) sc.getAttribute("memberDao");
+			
+			Member member = (Member) request.getAttribute("member");
+			memberDao.update(member);
+
+			request.setAttribute("viewUrl", "redirect:list.do");
+
+		} catch (Exception e) {
+			throw new ServletException();
+		}
+	}
+}
